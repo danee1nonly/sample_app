c0f86c51cc9fc769188b0c5a36a59023e7cbaa5e
Danny Yuan <dyn@mit.edu>
2011-01-12 01:20:40 -0500

    done w static, not really

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,13 @@
 class PagesController < ApplicationController
   def home
+    @title = "home"
   end
 
   def contact
+    @title = "Contact"
   end
 
+  def about
+    @title = "About"
+  end
 end
